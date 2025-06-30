005587b54fd3a57b0eb509eb090748d9216523f5
mr <mr.scrpt@gmail.com>
2025-06-30 16:57:23 +0300

    fix: redme

--- src/widgets/PromoSection/index.tsx
+++ src/widgets/PromoSection/index.tsx
@@ -8,23 +8,31 @@ export const PromoSection: FC<HTMLAttributes<HTMLDivElement>> = (props) => {
   const { className } = props;
 
   // Hardcoded values for non-multilingual project
-  const slogan = "Безпека та конфіденційність спілкування — пріоритет для нашої компанії. Все це надає нам Lecksis — месенджер майбутнього.";
-  const link = "https://lecksis.com/profile/stinger";
-  const text = "Ми у мессенджері";
+  const slogan =
+    "Безпека та конфіденційність спілкування — пріоритет для нашої компанії. Все це надає нам Lecksis — месенджер майбутнього.";
+  const link = "https://lecksis.com/";
   const name = "Lecksis";
 
   return (
-    <Section id="promo" className={className ? `py-16 bg-gray-100 ${className}` : "py-16 bg-gray-100"}>
+    <Section
+      id="promo"
+      className={
+        className ? `py-16 bg-gray-100 ${className}` : "py-16 bg-gray-100"
+      }
+    >
       <Container className="flex flex-col items-center justify-center p-[30px] gap-[15px] text-center md:p-[50px] lg:text-lg">
         <div>
-          <Image src="/images/icon_lecksis.svg" alt="Lecksis Logo" width={200} height={200} />
+          <Link href={link}>
+            <Image
+              src="/images/icon_lecksis.svg"
+              alt="Lecksis Logo"
+              width={200}
+              height={200}
+            />
+          </Link>
         </div>
         <div className="text-base lg:text-lg">
           <div>{slogan}</div>
-          <div>
-            <span>{text} </span>
-            <Link href={link}>{name}</Link>
-          </div>
         </div>
       </Container>
     </Section>
